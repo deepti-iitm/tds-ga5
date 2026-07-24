704b569f1f7ab7be72709ca69c52bb67fc473d68
deepti-iitm <23f2005361@ds.study.iitm.ac.in>
2026-07-24 19:01:26 +0530

    Change AIPIPE_TOKEN in config.py
    Updated AIPIPE_TOKEN for authentication.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 EMAIL = "23f2005361@ds.study.iitm.ac.in"
-AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIzZjIwMDUzNjFAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDgzMTA2NiwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NTQzNTg2Nn0.N_xKEB7TPpGgI8ly7F5sUIOaCDdgT0x4KNYNgBuSZnw"
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIzZjIwMDUzNjFAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDg5OTc4NywiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NTUwNDU4N30.gOembyFoKChlyn6vA5RwrlDe4F4uHzr9Tf4kEkyURNw"
 
 # Fixed — do not change
 AIPIPE_BASE = "https://aipipe.org/openai/v1"

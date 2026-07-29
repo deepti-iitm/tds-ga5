a9298aa0d4c76e0c02c3199582b08bb238252b00
deepti-iitm <23f2005361@ds.study.iitm.ac.in>
2026-07-29 10:49:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 EMAIL = "23f2005361@ds.study.iitm.ac.in"
-AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIzZjIwMDUzNjFAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDg5OTc4NywiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NTUwNDU4N30.gOembyFoKChlyn6vA5RwrlDe4F4uHzr9Tf4kEkyURNw"
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIzZjIwMDUzNjFAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NTI0ODk2NSwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NTg1Mzc2NX0.KTya0oTPrWzXDg2C0TrQUowXl0ar1I6VKkpiTe5rwXg"
 
 # Fixed — do not change
 AIPIPE_BASE = "https://aipipe.org/openai/v1"
